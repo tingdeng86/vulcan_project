e5ec6b5d4da80060b48e7072400f49dcd407cce7
alex <7253alex@gmail.com>
2022-02-23 01:20:28 -0800

    tablet nav draft

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Header from "./components/Header";
 import Gallery from "./components/Gallery";
 
 function App() {
-  function dosth(e) {
+  function closeNav(e) {
     if(e.target.tagName !== "HEADER" && e.target.tagName !== "SPAN" && document.body.className === "vsc-initialized show") {
       console.log(e.target.tagName);
       document.body.classList.remove('show');
     }
   }
   return (
-    <div id="wr" className="wrapper" onClick={e => dosth(e)}>
+    <div id="wr" className="wrapper" onClick={e => closeNav(e)}>
       <Header></Header>
       <Nav></Nav>
       <HeaderSlider/>

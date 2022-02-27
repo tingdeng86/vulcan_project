2e56267799e60425e00a2b58f7f0111f45155c71
alex <7253alex@gmail.com>
2022-02-26 18:40:44 -0800

    nav

---
+++ src/components/ProductAdvisor.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProductAdvisor() {
+  return (
+    <div className='product-advisor'>
+        ProductAdvisor
+    </div>
+  )
+}
+
+export default ProductAdvisor
